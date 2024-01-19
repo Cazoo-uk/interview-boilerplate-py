7e146d617da361d8eb8e987d60969f235a3a7997
SimonReekie <93524121+SimonReekie@users.noreply.github.com>
2024-01-19 15:46:50 +0000

    added new tests

--- src/app.py
+++ src/app.py
@@ -1,2 +1,9 @@
 def main(range_to: int) -> list:
-    return list(range(1, range_to + 1))
+    output = []
+    for i in range(1, range_to + 1):
+        if i == 3:
+            output.append("Cazoo!")
+        else:
+            output.append(i)
+
+    return output
--- test/test_app.py
+++ test/test_app.py
@@ -11,3 +11,15 @@ def test_should_return_input_length_twenty():
 
 def test_should_return_in_order():
     assert main(2) == [1, 2]
+
+
+def test_should_return_cazoo_for_third_item():
+    assert main(3) == [1, 2, "Cazoo!"]
+
+
+def test_should_return_cazoo_for_third_item_up_to_five():
+    assert main(5) == [1, 2, "Cazoo!", 4, 5]
+
+
+def test_should_return_cazoo_for_third_item_up_to_six():
+    assert main(5) == [1, 2, "Cazoo!", 4, 5, "Cazoo!"]
